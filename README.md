da171fdb1b5c1c76be1ddf1f66e9b61dc2f75574
jkgoates <jeremiahgoates@gmail.com>
2023-05-04 15:58:35 -0600

    Included DISLIN

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fortran-streamlines
+
+# To compile run:
+# gfortran .\common\dislin.f90 plot.f90 main.f90 -o main.exe  .\lib\dislin\dismg.a -luser32 -lgdi32 -lopengl32
--- test.f95
+++ test.f95
@@ -3,19 +3,24 @@ program array
 
     implicit none
  
-    real,dimension(:),allocatable :: my_array
-    integer :: M,N,i
+    real,dimension(:,:),allocatable :: my_array
+    integer :: M,N,i,j
 
+    
+    N = 4
     M = 100
-    allocate(my_array(M))
+
+    allocate(my_array(M,N))
 
 
     do i = 1, M 
-
-        my_array(i) = i
+        do j = 1, N
+            my_array(i,j) = i*j
+        end do
         
     end do
-
+    
+    write(unit,'(*())')
 
     write(*,*) my_array
 
